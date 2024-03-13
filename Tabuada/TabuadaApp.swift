b8fe80784876649343464d76cd9056aec5e08803
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-03-13 12:09:45 -0300

    Initial Commit

---
+++ Tabuada/TabuadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabuadaApp.swift
+//  Tabuada
+//
+//  Created by Igor Florentino on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TabuadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
